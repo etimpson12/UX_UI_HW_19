448ffb61792b81864b99c17a5f7fb3c1c63d7bfb
etimpson12 <100228243+etimpson12@users.noreply.github.com>
2022-07-26 14:55:16 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 console.log("Your index.js file is loaded corrently!");
 
-$("#Contact").on('click', function() {
+$(function (){
+    $("#Contact").on('click', function() {
         $('html, body').animate({
             scrollTop: $(".footercontent").offset().top
         }, 2000);
-});
+    });
+})
